09443b85a3a68a108261d7084d93a8efb8d05a7c
Matt Steele <orphum@gmail.com>
2010-10-23 22:49:24 -0500

    triangle.rb finished

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  check_violation(a,b,c)
   return :equilateral if a == b and b == c
   return :isosceles if a == b or b == c or a == c
   :scalene
+end
+
+def check_violation(*sides)
+  raise TriangleError if sides.select{|x| x <= 0}.size > 0
+  s = sides.sort
+  raise TriangleError if s[0] + s[1] <= s[2]
 end
 
 # Error class used in part 2.  No need to change this code.
